225c80a9847258477d7d281a40608765d22aa3cf
Горбунов Никита Иванович <nigorbunov@alfabank.ru>
2025-02-12 17:54:36 +0400

    Setup maven-publish

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,8 +1,30 @@
 plugins {
     `java-library`
+    `maven-publish`
 }
+
+group = "com.example"
+version = "1.0"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "Release"
+            url = uri("https://example.com/repository")
+            credentials {
+                username = "any_username"
+                password = "any_password"
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("Release") {
+            from(components["java"])
+        }
+    }
 }
